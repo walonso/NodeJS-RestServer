1eee1c28ecdcef626ba37c1d619fd988ea778b4a
walonsor <walteralonso20@yahoo.com>
2018-11-19 21:49:24 -0500

    implementacion update, get, delete y coneccion a bd mongo en mlab

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // -----------------------
 process.env.PORT = process.env.PORT || 3000;
+
+// -----------------------
+// Entorno
+// -----------------------
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// -----------------------
+// Base de datos
+// -----------------------
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://cafe-user:123456q@ds261302.mlab.com:61302/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/usuario');
 const app = express();
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    //Usuario.find({ google: true })
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                })
+
+            })
+
+
+        })
+        //res.json('get Usuario')
 })
 
 app.post('/usuario', function(req, res) {
@@ -39,13 +69,74 @@ app.post('/usuario', function(req, res) {
 app.put('/usuario/:id', function(req, res) {
 
     let id = req.params.id;
-    res.json({
-        id
+    //el segundo parametro son las propiedades que se quieren actualizar
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    //forma de quitar atributos para que nos los actualice
+    /*delete body.password;
+    delete body.google;*/
+
+    /*Usuario.findById(id, (err, usuarioDB) => {
+        usuarioDB.save();
+    })*/
+
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+
     })
+
+
+
 })
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    let cambiaEstado = {
+        estado: false
+    }
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    });
+
+
+
+    //res.json('delete Usuario')
 })
 
 module.exports = app;
